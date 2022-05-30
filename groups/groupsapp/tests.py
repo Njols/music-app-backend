7014234393ad1c9524627abcf2b2e21444cd2bbb
Njols <niels@tonsoftime.com>
2022-05-30 14:16:06 +0200

    added tests

--- groups/groupsapp/tests.py
+++ groups/groupsapp/tests.py
@@ -1,3 +1,53 @@
-from django.test import TestCase
+import unittest
+import os
+from django.contrib.auth.hashers import check_password, make_password
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+import sys
+
+sys.path.append("../")
+import groups
+
+django.setup()
+from .models import User, Group
+
+
+class TestCaseGoup(unittest.TestCase):
+    def test_constructor(self):
+        user = User(user_id=1, username="TestGuy")
+        group = Group(
+            name="TestGroup", picture_url="https://www.google.com", owner=user
+        )
+        self.assertEqual(group.name, "TestGroup")
+        self.assertEqual(group.picture_url, "https://www.google.com")
+        self.assertEqual(group.owner, user)
+
+    def test_name(self):
+        user = User(user_id=1, username="TestGuy")
+        group = Group(
+            name="TestGroup", picture_url="https://www.google.com", owner=user
+        )
+        group.name = "NewName"
+        self.assertEqual(group.name, "NewName")
+
+    def test_picture_url(self):
+        user = User(user_id=1, username="TestGuy")
+        group = Group(
+            name="TestGroup", picture_url="https://www.google.com", owner=user
+        )
+        group.picture_url = "https://www.facebook.com"
+        self.assertEqual(group.picture_url, "https://www.facebook.com")
+
+    def test_owner(self):
+        user = User(user_id=1, username="TestGuy")
+        group = Group(
+            name="TestGroup", picture_url="https://www.google.com", owner=user
+        )
+        new_user = User(user_id=2, username="NewGuy")
+        group.owner = new_user
+        self.assertEqual(group.owner, new_user)
+
+
+if __name__ == "__main__":
+    unittest.main()
--- users/usersapp/tests.py
+++ users/usersapp/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+import unittest
+import os
+from django.contrib.auth.hashers import check_password, make_password
 
-# Create your tests here.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+import django
+import sys
+
+sys.path.append("../")
+import users
+
+django.setup()
+from .models import User
+
+
+class TestCaseUser(unittest.TestCase):
+    def test_constructor(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        self.assertEqual(user.username, "TestGuy")
+        self.assertEqual(user.email, "testguy@gmail.com")
+
+    def test_set_password(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        user.set_password("NewPass")
+        self.assertTrue(check_password("NewPass", user.password))
+
+    def test_check_password_incorrect(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        self.assertFalse(user.check_password("FalsePassword"))
+
+    def test_check_password_correct(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        user.set_password("NewPass")
+        result = user.check_password("NewPass")
+        self.assertTrue(result)
+
+    def test_username(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        user.username = "NewUsername"
+        self.assertEqual(user.username, "NewUsername")
+
+    def test_email(self):
+        user = User(username="TestGuy", email="testguy@gmail.com", password="TestPass")
+        user.email = "newemail@gmail.com"
+        self.assertEqual(user.email, "newemail@gmail.com")
+
+
+if __name__ == "__main__":
+    unittest.main()
